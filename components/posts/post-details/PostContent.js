0d561e6e40ed3ec0da6fc9f4f04d0275ef771266
Ilian Rusev <ilianrusev@gmail.com>
2023-01-30 22:43:12 +0200

    render code snippets from markdown with syntax highlighter

--- components/posts/post-details/PostContent.js
+++ components/posts/post-details/PostContent.js
@@ -1,4 +1,7 @@
 import Image from "next/image";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
+
 import ReactMarkdown from "react-markdown";
 import styles from "./PostContent.module.css";
 import PostHeader from "./PostHeader";
@@ -15,6 +18,17 @@ function PostContent(props) {
           alt={image.alt}
           width={600}
           height={300}
+        />
+      );
+    },
+    code(code) {
+      const { className, children } = code;
+      const language = className.split("-")[1]; // className is something like language-js => We need the "js" part here
+      return (
+        <SyntaxHighlighter
+          style={atomDark}
+          language={language}
+          children={children}
         />
       );
     },
