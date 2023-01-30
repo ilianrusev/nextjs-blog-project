2d745bb36a07b2f435ab4d4ce332c76a613044aa
Ilian Rusev <ilianrusev@gmail.com>
2023-01-30 22:35:51 +0200

    render images with next image from markdown

--- components/posts/post-details/PostContent.js
+++ components/posts/post-details/PostContent.js
@@ -1,16 +1,29 @@
+import Image from "next/image";
 import ReactMarkdown from "react-markdown";
 import styles from "./PostContent.module.css";
 import PostHeader from "./PostHeader";
 
 function PostContent(props) {
-  console.log(props);
   const { post } = props;
   const imagePath = `/images/${post.image} `;
+
+  const customRenderers = {
+    img(image) {
+      return (
+        <Image
+          src={`/images/${image.src}`}
+          alt={image.alt}
+          width={600}
+          height={300}
+        />
+      );
+    },
+  };
 
   return (
     <article className={styles.content}>
       <PostHeader title={post.title} image={imagePath} />
-      <ReactMarkdown>{post.content}</ReactMarkdown>
+      <ReactMarkdown components={customRenderers}>{post.content}</ReactMarkdown>
     </article>
   );
 }
