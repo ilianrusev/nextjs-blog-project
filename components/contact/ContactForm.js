d451c0de95267f5cdd3f4e8db0bc8a47c532807e
Ilian Rusev <ilianrusev@gmail.com>
2023-01-30 23:18:23 +0200

    sending data from client to api route

--- components/contact/ContactForm.js
+++ components/contact/ContactForm.js
@@ -1,22 +1,59 @@
+import { useState } from "react";
 import styles from "./ContactForm.module.css";
 
 function ContactForm() {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [message, setMessage] = useState("");
+
+  function sendMessageHandler(event) {
+    event.preventDefault();
+
+    //optional add client side validation
+
+    fetch("/api/contacts", {
+      method: "POST",
+      body: JSON.stringify({ email, name, message }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+
   return (
-    <section className={styles.contact}>
+    <section className={styles.contact} onSubmit={sendMessageHandler}>
       <h1>How can I help you</h1>
       <form className={styles.form}>
         <div className={styles.controls}>
           <div className={styles.control}>
             <label htmlFor="email">Your Email</label>
-            <input type="email" id="email" required />
+            <input
+              type="email"
+              id="email"
+              required
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+            />
           </div>
           <div className={styles.control}>
             <label htmlFor="name">Your Name</label>
-            <input type="text" id="name" required />
+            <input
+              type="text"
+              id="name"
+              required
+              value={name}
+              onChange={(event) => setName(event.target.value)}
+            />
           </div>
           <div className={styles.control}>
             <label htmlFor="email">Your Message</label>
-            <textarea rows={"5"} id="email"></textarea>
+            <textarea
+              rows={"5"}
+              id="email"
+              required
+              value={message}
+              onChange={(event) => setMessage(event.target.value)}
+            ></textarea>
           </div>
           <div className={styles.actions}>
             <button>Send Message</button>
